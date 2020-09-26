44804f1c455f96324746b1c8134835645ea8a2cf
Emmef <git@emmef.org>
2020-09-26 23:40:01 +0200

    Added Index::safe, Inclusive::safe, Index::unsafe and Inclusive::unsafe test coverage.

--- test/core/Index-tests.cc
+++ test/core/Index-tests.cc
@@ -14,7 +14,7 @@ static constexpr size_t ONE = 1;
 
 BOOST_AUTO_TEST_SUITE(org_simple_core_Index)
 
-BOOST_AUTO_TEST_CASE(testIndexCheckedZeroSizeZeroFails) {
+BOOST_AUTO_TEST_CASE(testIndexCheckedZeroSizeZeroThrows) {
   [[maybe_unused]] size_t out;
   BOOST_CHECK_THROW(out = Exclusive::checked(ZERO, ZERO), std::out_of_range);
 }
@@ -120,6 +120,105 @@ BOOST_AUTO_TEST_CASE(testInclusiveUnCheckedSizeDoesNotThrow) {
 
 BOOST_AUTO_TEST_CASE(testInclusiveUnCheckedSizePlusOneDoesNotThrow) {
   BOOST_CHECK_EQUAL(SIZE + ONE, Inclusive::unchecked(SIZE + ONE, SIZE));
+}
+
+/**
+ * Tests for safe and unsafe do not include situations that can throw exception.
+ * Creating multiple test sources with different values for
+ * ORG_SIMPLE_INDEX_POLICY_FORCE_SAFE_UNCHECKED and
+ * ORG_SIMPLE_INDEX_POLICY_FORCE_UNSAFE_CHECKED does not work.
+ */
+
+BOOST_AUTO_TEST_CASE(testIndexSafeZeroArgument) {
+  BOOST_CHECK_EQUAL(0, Exclusive::safe(ZERO, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testIndexSafeNonZeroArgument) {
+  size_t value = 3;
+  BOOST_CHECK_EQUAL(value, Exclusive::safe(value, SIZE));
+  value = 5;
+  BOOST_CHECK_EQUAL(value, Exclusive::safe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testIndexSafeSizeMinusOne) {
+  size_t value = SIZE - 1;
+  BOOST_CHECK_EQUAL(value, Exclusive::safe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testIndexUnsafeZeroSizeZero) {
+  BOOST_CHECK_EQUAL(ZERO, Exclusive::unsafe(ZERO, ZERO));
+}
+
+BOOST_AUTO_TEST_CASE(testIndexUnsafeZeroArgument) {
+  BOOST_CHECK_EQUAL(0, Exclusive::unsafe(ZERO, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testIndexUnsafeNonZeroArgument) {
+  size_t value = 3;
+  BOOST_CHECK_EQUAL(value, Exclusive::unsafe(value, SIZE));
+  value = 5;
+  BOOST_CHECK_EQUAL(value, Exclusive::unsafe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testIndexUnsafeSizeMinusOne) {
+  size_t value = SIZE - 1;
+  BOOST_CHECK_EQUAL(value, Exclusive::unsafe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testIndexUnsafeSizeDoesNotThrow) {
+  BOOST_CHECK_EQUAL(SIZE, Exclusive::unsafe(SIZE, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveSafeZeroSizeZero) {
+  BOOST_CHECK_EQUAL(ZERO, Inclusive::safe(ZERO, ZERO));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveSafeZeroArgument) {
+  BOOST_CHECK_EQUAL(0, Inclusive::safe(ZERO, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveSafeNonZeroArgument) {
+  size_t value = 3;
+  BOOST_CHECK_EQUAL(value, Inclusive::safe(value, SIZE));
+  value = 5;
+  BOOST_CHECK_EQUAL(value, Inclusive::safe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveSafeSizeMinusOne) {
+  size_t value = SIZE - 1;
+  BOOST_CHECK_EQUAL(value, Inclusive::safe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveSafeSize) {
+  BOOST_CHECK_EQUAL(SIZE, Inclusive::safe(SIZE, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveUnsafeZeroSizeZero) {
+  BOOST_CHECK_EQUAL(ZERO, Inclusive::unsafe(ZERO, ZERO));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveUnsafeZeroArgument) {
+  BOOST_CHECK_EQUAL(0, Inclusive::unsafe(ZERO, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveUnsafeNonZeroArgument) {
+  size_t value = 3;
+  BOOST_CHECK_EQUAL(value, Inclusive::unsafe(value, SIZE));
+  value = 5;
+  BOOST_CHECK_EQUAL(value, Inclusive::unsafe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveUnsafeSizeMinusOne) {
+  size_t value = SIZE - 1;
+  BOOST_CHECK_EQUAL(value, Inclusive::unsafe(value, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveUnsafeSizeDoesNotThrow) {
+  BOOST_CHECK_EQUAL(SIZE, Inclusive::unsafe(SIZE, SIZE));
+}
+
+BOOST_AUTO_TEST_CASE(testInclusiveUnsafeSizePlusOneDoesNotThrow) {
+  BOOST_CHECK_EQUAL(SIZE + ONE, Inclusive::unsafe(SIZE + ONE, SIZE));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
