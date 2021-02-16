c0ba77226dce51709a2fee6b2bbe89a44528094e
Emmef <git@emmef.org>
2021-02-16 22:39:05 +0100

    Added SampleLayout.h and SampleLayoutTests.cc to convert between channel-buffers and frames or consecutive interleaved samples.

---
+++ include/org-simple/util/SampleLayout.h
@@ -0,0 +1,165 @@
+#ifndef ORG_SIMPLE_SAMPLELAYOUT_H
+#define ORG_SIMPLE_SAMPLELAYOUT_H
+/*
+ * org-simple/util/SampleLayout.h
+ *
+ * Added by michel on 2021-02-16
+ * Copyright (C) 2015-2021 Michel Fleur.
+ * Source https://github.com/emmef/org-simple
+ * Email org-simple@emmef.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <org-simple/util/NumArray.h>
+
+namespace org::simple::util {
+
+struct SampleLayout {
+
+  /**
+   * Moves samples from CHANNEL channel-buffers with each FRAME samples to an
+   * interleaved output, where samples are ordered to FRAME and then CHANNEL.
+   * For example, if there are two channels with two frames :
+   * <pre>
+   *   channel 0: 11 12
+   *   channel 1: 21 22
+   * </pre>
+   * The output would look like
+   * <pre>
+   *   11 21 12 22
+   * </pre>
+   * @tparam S Type of samples.
+   * @tparam CHANNELS The number of channels per frame.
+   * @tparam FRAMES The number of frames.
+   * @param channel_ptr pointer to CHANNEL pointers to buffers of FRAME samples
+   * each. The channel pointer are updated such that this method can be invoked
+   * in a loop.
+   * @param interleaved_output a consecutive buffer of CHANNEL * FRAME samples.
+   * The output pointer is updates such that this method can be invoked in a
+   * loop.
+   */
+  template <typename S, size_t CHANNELS, size_t FRAMES>
+  static void channel_buffers_to_interleaved(S const **channel_ptr,
+                                             S *&interleaved_output) noexcept {
+    for (size_t frame = 0; frame < FRAMES; frame++) {
+      for (size_t channel = 0; channel < CHANNELS; channel++) {
+        *interleaved_output++ = *channel_ptr[channel]++;
+      }
+    }
+  }
+
+  /**
+   * Moves samples from CHANNEL channel-buffers with each FRAME samples to a
+   * consecutive array of FRAME frames with each CHANNELS samples. For example,
+   * if there are two channels with two frames :
+   * <pre>
+   *   channel 0: 11 12
+   *   channel 1: 21 22
+   * </pre>
+   * The output would look like
+   * <pre>
+   *   Frame 0: 11 21
+   *   Frame 1: 12 22
+   * </pre>
+   * @tparam S Type of samples.
+   * @tparam CHANNELS The number of channels per frame.
+   * @tparam FRAMES The number of frames.
+   * @param channel_ptr pointer to CHANNEL pointers to buffers of FRAME samples
+   * each. The channel pointer are updated such that this method can be invoked
+   * in a loop.
+   * @param frames a consecutive buffer of FRAME frames with each CHANNELS
+   * samples. The buffer is updates such that this method can be invoked in a
+   * loop.
+   */
+  template <typename S, size_t CHANNELS, size_t FRAMES>
+  static void
+  channel_buffers_to_frames(S const **channel_ptr,
+                            NumArray<S, CHANNELS> *&frames) noexcept {
+    for (size_t frame = 0; frame < FRAMES; frame++) {
+      NumArray<S, CHANNELS> &output = *frames++;
+      for (size_t channel = 0; channel < CHANNELS; channel++) {
+        output[channel] = *channel_ptr[channel]++;
+      }
+    }
+  }
+
+  /**
+   * Moves samples from an interleaved input, where samples are ordered by frame
+   * and then channel, to CHANNEL channel-buffers with each FRAME samples. For
+   * example, if the input looks like :
+   * <pre>
+   *   11 21 12 22
+   * </pre>
+   * The channel buffers would look like:
+   * <pre>
+   *   channel 0: 11 12
+   *   channel 1: 21 22
+   * </pre>
+   *
+   * @tparam S Type of samples.
+   * @tparam CHANNELS The number of channels per frame.
+   * @tparam FRAMES The number of frames.
+   * @param interleaved_input a consecutive buffer of CHANNEL * FRAME samples.
+   * The output pointer is updates such that this method can be invoked in a
+   * loop.
+   * @param channel_ptr channel_ptr pointer to CHANNEL pointers to buffers of
+   * FRAME samples each. The channel pointer are updated such that this method
+   * can be invoked in a loop.
+   */
+  template <typename S, size_t CHANNELS, size_t FRAMES>
+  static void interleaved_to_channel_buffers(const S *&interleaved_input,
+                                             S **channel_ptr) noexcept {
+    for (size_t frame = 0; frame < FRAMES; frame++) {
+      for (size_t channel = 0; channel < CHANNELS; channel++) {
+        *channel_ptr[channel]++ = *interleaved_input++;
+      }
+    }
+  }
+
+  /**
+   * Move sampples from a consecutive array of FRAME frames with each CHANNELS samples, to CHANNEL channel-buffers with each FRAME samples. For
+   * example, if the input looks like :
+   * <pre>
+   *   Frame 0: 11 21
+   *   Frame 1: 12 22
+   * </pre>
+   * The channel buffer would look like:
+   * <pre>
+   *   channel 0: 11 12
+   *   channel 1: 21 22
+   * </pre>
+   * @tparam S Type of samples.
+   * @tparam CHANNELS The number of channels per frame.
+   * @tparam FRAMES The number of frames.
+   * @param interleaved_input
+   * @param channel_ptr channel_ptr pointer to CHANNEL pointers to buffers of
+   * FRAME samples each. The channel pointer are updated such that this method
+   * can be invoked in a loop.
+   */
+  template <typename S, size_t CHANNELS, size_t FRAMES>
+  static void interleaved_to_channel_buffers(
+      const NumArray<S, CHANNELS> *&interleaved_input,
+      S **channel_ptr) noexcept {
+    for (size_t frame = 0; frame < FRAMES; frame++) {
+      const NumArray<S, CHANNELS> &input = *interleaved_input++;
+      for (size_t channel = 0; channel < CHANNELS; channel++) {
+        *channel_ptr[channel]++ = input[channel];
+      }
+    }
+  }
+};
+
+} // namespace org::simple::util
+
+#endif // ORG_SIMPLE_SAMPLELAYOUT_H
---
+++ test/util/SampleLayoutTests.cc
@@ -0,0 +1,171 @@
+//
+// Created by michel on 16-02-21.
+//
+
+#include "test-helper.h"
+#include <org-simple/util/SampleLayout.h>
+#include <sstream>
+#include <string>
+
+using Layout = org::simple::util::SampleLayout;
+
+namespace {
+
+static constexpr size_t CHANNELS = 3;
+static constexpr size_t FRAMES = 5;
+static constexpr size_t SAMPLES = CHANNELS * FRAMES;
+
+std::string print_interleaved(const int *const samples) {
+  std::stringstream out;
+
+  for (size_t i = 0; i < SAMPLES; i++) {
+    out << " " << samples[i];
+  }
+  return out.str();
+}
+
+std::string
+print_interleaved(const org::simple::util::NumArray<int, CHANNELS> *frames) {
+  std::stringstream out;
+
+  for (size_t i = 0; i < FRAMES; i++) {
+    for (size_t j = 0; j < CHANNELS; j++) {
+      out << " " << frames[i][j];
+    }
+  }
+  return out.str();
+}
+
+std::string
+print_interleaved(const int ** frames) {
+  std::stringstream out;
+
+  for (size_t f = 0; f < FRAMES; f++) {
+    for (size_t c = 0; c < CHANNELS; c++) {
+      out << " " << frames[c][f];
+    }
+  }
+  return out.str();
+}
+
+static const int channel_buffer_samples[CHANNELS][FRAMES] = {
+    {11, 12, 13, 14, 15}, {21, 22, 23, 24, 25}, {31, 32, 33, 34, 35}};
+
+const int interleaved_samples[SAMPLES] = {11, 21, 31, 12, 22, 32, 13, 23,
+                                          33, 14, 24, 34, 15, 25, 35};
+
+const org::simple::util::NumArray<int, CHANNELS> sample_frames[FRAMES] =
+    {{11, 21, 31}, {12, 22, 32}, {13, 23, 33}, {14, 24, 34}, {15, 25, 35}};
+
+} // namespace
+
+BOOST_AUTO_TEST_SUITE(org_simple_util_SampleLayout)
+
+BOOST_AUTO_TEST_CASE(testChannelBuffersToInterleavedArray) {
+  int output[SAMPLES];
+  for (size_t i = 0; i < SAMPLES; i++) {
+    output[i] = 0;
+  }
+
+  int *output_ptr = &output[0];
+  const int *channel_start[CHANNELS];
+  const int *channel_walk[CHANNELS];
+  channel_start[0] = channel_walk[0] = &channel_buffer_samples[0][0];
+  channel_start[1] = channel_walk[1] = &channel_buffer_samples[1][0];
+  channel_start[2] = channel_walk[2] = &channel_buffer_samples[2][0];
+  const int **channel_ptr = &channel_walk[0];
+  Layout::channel_buffers_to_interleaved<int, CHANNELS, FRAMES>(channel_ptr,
+                                                                output_ptr);
+
+  std::string output_string = print_interleaved(output);
+  std::string expected_string = print_interleaved(interleaved_samples);
+
+  BOOST_CHECK_EQUAL(output_string, expected_string);
+  for (size_t i = 0; i < CHANNELS; i++) {
+    BOOST_CHECK_EQUAL(channel_walk[i] - channel_start[i], FRAMES);
+  }
+  BOOST_CHECK_EQUAL(output_ptr - &output[0], SAMPLES);
+}
+
+BOOST_AUTO_TEST_CASE(testChannelBuffersToFrames) {
+  org::simple::util::NumArray<int, CHANNELS> output[FRAMES];
+
+  for (size_t i = 0; i < FRAMES; i++) {
+    output[i].zero();
+  }
+
+  org::simple::util::NumArray<int, CHANNELS> *output_ptr = &output[0];
+
+  const int *channel_start[CHANNELS];
+  const int *channel_walk[CHANNELS];
+  channel_start[0] = channel_walk[0] = &channel_buffer_samples[0][0];
+  channel_start[1] = channel_walk[1] = &channel_buffer_samples[1][0];
+  channel_start[2] = channel_walk[2] = &channel_buffer_samples[2][0];
+  const int **channel_ptr = &channel_walk[0];
+  Layout::channel_buffers_to_frames<int, CHANNELS, FRAMES>(channel_ptr,
+                                                                output_ptr);
+
+  std::string output_string = print_interleaved(output);
+  std::string expected_string = print_interleaved(interleaved_samples);
+
+  BOOST_CHECK_EQUAL(output_string, expected_string);
+
+  BOOST_CHECK_EQUAL(output_string, expected_string);
+  for (size_t i = 0; i < CHANNELS; i++) {
+    BOOST_CHECK_EQUAL(channel_walk[i] - channel_start[i], FRAMES);
+  }
+  BOOST_CHECK_EQUAL(output_ptr - &output[0], FRAMES);
+}
+
+BOOST_AUTO_TEST_CASE(testInterleavedArrayToChannelBuffers) {
+
+  int channel_buffer_output[CHANNELS][FRAMES] = {
+      { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }};
+
+  int *channel_start[CHANNELS];
+  int *channel_walk[CHANNELS];
+  channel_start[0] = channel_walk[0] = &channel_buffer_output[0][0];
+  channel_start[1] = channel_walk[1] = &channel_buffer_output[1][0];
+  channel_start[2] = channel_walk[2] = &channel_buffer_output[2][0];
+  int **channel_ptr = &channel_walk[0];
+
+  const int * interleaved_input = &interleaved_samples[0];
+  Layout::interleaved_to_channel_buffers<int, CHANNELS, FRAMES>(interleaved_input,
+                                                                channel_ptr);
+
+  std::string output_string = print_interleaved(const_cast<const int **>(&channel_start[0]));
+  std::string expected_string = print_interleaved(interleaved_samples);
+
+  BOOST_CHECK_EQUAL(output_string, expected_string);
+  for (size_t i = 0; i < CHANNELS; i++) {
+    BOOST_CHECK_EQUAL(channel_walk[i] - channel_start[i], FRAMES);
+  }
+  BOOST_CHECK_EQUAL(interleaved_input - interleaved_samples, SAMPLES);
+}
+
+BOOST_AUTO_TEST_CASE(testFramesToChannelBuffers) {
+  int channel_buffer_output[CHANNELS][FRAMES] = {
+      { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }};
+
+  int *channel_start[CHANNELS];
+  int *channel_walk[CHANNELS];
+  channel_start[0] = channel_walk[0] = &channel_buffer_output[0][0];
+  channel_start[1] = channel_walk[1] = &channel_buffer_output[1][0];
+  channel_start[2] = channel_walk[2] = &channel_buffer_output[2][0];
+  int **channel_ptr = &channel_walk[0];
+
+  const org::simple::util::NumArray<int, CHANNELS> *input_ptr = &sample_frames[0];
+
+  Layout::interleaved_to_channel_buffers<int, CHANNELS, FRAMES>(input_ptr,
+                                                                channel_ptr);
+  std::string output_string = print_interleaved(const_cast<const int **>(&channel_start[0]));
+  std::string expected_string = print_interleaved(interleaved_samples);
+
+  BOOST_CHECK_EQUAL(output_string, expected_string);
+  for (size_t i = 0; i < CHANNELS; i++) {
+    BOOST_CHECK_EQUAL(channel_walk[i] - channel_start[i], FRAMES);
+  }
+  BOOST_CHECK_EQUAL(input_ptr - &sample_frames[0], FRAMES);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
