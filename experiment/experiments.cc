fd2d0012619d6653f844c613976eb99927c21c87
Emmef <git@emmef.org>
2021-03-29 23:42:38 +0200

    experiments.cc: added (it would break compile not to)

---
+++ experiment/experiments.cc
@@ -0,0 +1,13 @@
+//
+// Created by michel on 27-03-21.
+//
+
+#include <iostream>
+#include <org-simple/dsp/iir-coefficients.h>
+
+using namespace std;
+
+int main(int , char **) {
+  cout << "Size of long double is " << sizeof(long double) << endl;
+  return 0;
+}
