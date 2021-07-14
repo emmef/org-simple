798f1107fc48a4962e51019bfdd6a436a0c92c28
Emmef <git@emmef.org>
2021-07-14 23:54:12 +0200

    FakeClock.h: Added features: - Added a choice between a thread-local or non-thread-local clock. - Added registering a callback that is called when clock is only read (not set or updated and read). - Added test-coverage for these features.

--- include/org-simple/util/FakeClock.h
+++ include/org-simple/util/FakeClock.h
@@ -25,41 +25,92 @@
 #include <cstdint>
 
 namespace org::simple::util {
-struct FakeClock {
+
+namespace helper {
+
+template <bool threadLocal> class FakeClockCounter;
+template <> class FakeClockCounter<false> {
+protected:
+  static uint64_t &counter() {
+    static uint64_t counter_;
+    return counter_;
+  }
+};
+template <> class FakeClockCounter<true> {
+protected:
+  static uint64_t &counter() {
+    static thread_local uint64_t counter_;
+    return counter_;
+  }
+};
+
+template <bool threadLocal>
+struct AbstractFakeClock : private FakeClockCounter<threadLocal> {
+  typedef bool (*get_callback_function)(uint64_t now, void *);
+
   typedef std::chrono::nanoseconds duration;
   typedef duration::rep rep;
   typedef duration::period period;
-  typedef std::chrono::time_point<FakeClock, duration> time_point;
+  typedef std::chrono::time_point<AbstractFakeClock, duration> time_point;
 
-  static_assert(FakeClock::duration::min() < FakeClock::duration::zero(),
+  static_assert(AbstractFakeClock::duration::min() <
+                AbstractFakeClock::duration::zero(),
                 "a clock's minimum duration cannot be less than its epoch");
 
   static constexpr bool is_steady = false;
 
-  static time_point now() { return time_point() + duration(get_count()); };
+  static uint64_t get_count() {
+    uint64_t now = FakeClockCounter<threadLocal>::counter();
+    auto cb = callback();
+    if (cb) {
+      auto d = data();
+      if (cb(now, d)) {
+        callback(true, nullptr);
+        data(true, nullptr);
+      }
+    }
+    return now;
+  }
 
-  static void set_now(duration ticks_since_epoch) {
-    set_count(ticks_since_epoch.count());
-  };
+  static time_point now() { return time_point() + duration(get_count()); };
 
   template <typename R, typename P>
   static void set_now(std::chrono::duration<R, P> ticks_since_epoch) {
     set_count(std::chrono::duration_cast<duration>(ticks_since_epoch).count());
   };
 
+  /**
+   * Sets a callback that is used when #get_count() or #now() is invoked.
+   *
+   * The callback gets the current tick plus the data provided in \c
+   * callback_data as arguments. If the callback returns \c true, the callback
+   * is immediately removed.
+   *
+   * @param get_callback
+   * @param callback_data
+   */
+  static void set_callback(get_callback_function function, void *callback_data) {
+    if (function) {
+      callback(true, function);
+      data(true, callback_data);
+    }
+    else {
+      callback(true, nullptr);
+      data(true, callback_data);
+    }
+  }
+
+  static void set_now(duration ticks_since_epoch) {
+    set_count(ticks_since_epoch.count());
+  };
+
   static void set_now(time_point time) {
     set_count(time.time_since_epoch().count());
   };
 
-  static void set_count(int64_t new_count) {
-    control_count(new_count, Control::SET);
-  }
+  static void set_count(int64_t new_count) { set(new_count); }
 
-  static int64_t get_count() { return control_count(0, Control::GET); }
-
-  static int64_t add_get_count(int64_t value) {
-    return control_count(value, Control::ADD);
-  }
+  static int64_t add_get_count(int64_t value) { return add_and_get(value); }
 
   // Map to C API
   static std::time_t to_time_t(const time_point &__t) {
@@ -68,27 +119,51 @@ struct FakeClock {
   }
 
   static time_point from_time_t(std::time_t __t) {
-    typedef std::chrono::time_point<FakeClock, std::chrono::seconds> __from;
-    return time_point_cast<FakeClock::duration>(
+    typedef std::chrono::time_point<AbstractFakeClock, std::chrono::seconds>
+        __from;
+    return time_point_cast<AbstractFakeClock::duration>(
         __from(std::chrono::seconds(__t)));
   }
 
 private:
-  enum class Control { SET, GET, ADD };
-  static int64_t control_count(int64_t value, Control ctrl) {
-    static thread_local int64_t count_;
-    switch (ctrl) {
-    case Control::SET:
-      count_ = value;
-      break;
-    case Control::ADD:
-      count_ += value;
-    case Control::GET:
-      break;
+  static get_callback_function callback(bool set = false, get_callback_function f = nullptr) {
+    static std::atomic<get_callback_function> callback_;
+    if (set) {
+      callback_ = f;
+      return nullptr;
     }
-    return count_;
+    else {
+      return callback_;
+    }
+  }
+
+  static void * data(bool set = false, void *d = nullptr) {
+    static std::atomic<void *> data_;
+    if (set) {
+      data_ = d;
+      return nullptr;
+    }
+    else {
+      return data_;
+    }
+  }
+
+  static bool (*get_callback_)(uint64_t now, void *);
+  static void *callback_data_;
+
+  static uint64_t add_and_get(uint64_t value) {
+    return (FakeClockCounter<threadLocal>::counter() += value);
+  }
+
+  static void set(uint64_t value) {
+    FakeClockCounter<threadLocal>::counter() = value;
   }
 };
+
+} // end of namespace helper
+
+typedef helper::AbstractFakeClock<false> FakeClock;
+typedef helper::AbstractFakeClock<true> FakeClockThreadLocal;
 
 } // namespace org::simple::util
 
--- test/util/FakeClock-tests.cc
+++ test/util/FakeClock-tests.cc
@@ -28,10 +28,39 @@ static Clock::duration generate_fake_duration_since_epoch() {
   return Clock::duration(count);
 }
 
+static std::atomic<uint64_t> test_callback_counter;
+static std::atomic<void *> data_set_by_callback;
+
+static void *random_callback_data() {
+  static constexpr size_t SIZE = 1024;
+  static char *ptr = new char[SIZE];
+
+  char *result = ptr + (rand() % SIZE);
+  return static_cast<void *>(result);
+}
+
+static bool callback_reset(uint64_t now, void *data) {
+  test_callback_counter = now;
+  data_set_by_callback = data;
+  return true;
+}
+
+static bool callback_no_reset(uint64_t now, void *data) {
+  return !callback_reset(now, data);
+}
+
+static void reset_clock_and_test_data() {
+  data_set_by_callback = nullptr;
+  test_callback_counter = 0;
+  Clock::set_callback(nullptr, nullptr);
+  Clock::set_count(0);
+}
+
 BOOST_AUTO_TEST_SUITE(org_simple_util_FakeClock)
 
 BOOST_AUTO_TEST_CASE(testNowIsAsSetUsingSystemDuration) {
-  const std::chrono::duration system_duration_since_epoch = generate_system_duration_since_epoch();
+  const std::chrono::duration system_duration_since_epoch =
+      generate_system_duration_since_epoch();
 
   Clock::set_now(system_duration_since_epoch);
 
@@ -71,6 +100,68 @@ BOOST_AUTO_TEST_CASE(testNowIsAsSetUsingTimepoint) {
   Clock::set_now(timePoint);
 
   BOOST_CHECK(timePoint == Clock::now());
+}
+
+BOOST_AUTO_TEST_CASE(testCallbackNoReset) {
+  void *initial_data = random_callback_data();
+  void *registered_callback_data = random_callback_data();
+  reset_clock_and_test_data();
+
+  data_set_by_callback = initial_data;
+  Clock::set_callback(callback_no_reset, registered_callback_data);
+
+  BOOST_CHECK_EQUAL(data_set_by_callback, initial_data);
+
+  uint64_t fake_time_set = rand();
+  Clock::set_count(fake_time_set);
+  uint64_t now = Clock::get_count();
+
+  BOOST_CHECK_EQUAL(now, fake_time_set);
+  BOOST_CHECK_EQUAL(test_callback_counter, fake_time_set);
+  BOOST_CHECK_EQUAL(data_set_by_callback, registered_callback_data);
+
+  data_set_by_callback = initial_data;
+  uint64_t fake_time_set_after_callback_no_reset = rand();
+  Clock::set_count(fake_time_set_after_callback_no_reset);
+  uint64_t now_after_callback_auto_reset = Clock::get_count();
+
+  BOOST_CHECK_EQUAL(now_after_callback_auto_reset, fake_time_set_after_callback_no_reset);
+  BOOST_CHECK_EQUAL(test_callback_counter, fake_time_set_after_callback_no_reset);
+  BOOST_CHECK_EQUAL(data_set_by_callback, registered_callback_data);
+
+
+  reset_clock_and_test_data();
+}
+
+BOOST_AUTO_TEST_CASE(testCallbackReset) {
+  void *initial_data = random_callback_data();
+  void *registered_callback_data = random_callback_data();
+
+  reset_clock_and_test_data();
+  data_set_by_callback = initial_data;
+
+  Clock::set_callback(callback_reset, registered_callback_data);
+
+  BOOST_CHECK_EQUAL(data_set_by_callback, initial_data);
+
+  uint64_t fake_time_set = rand();
+  Clock::set_count(fake_time_set);
+  uint64_t now = Clock::get_count();
+
+  BOOST_CHECK_EQUAL(now, fake_time_set);
+  BOOST_CHECK_EQUAL(test_callback_counter, fake_time_set);
+  BOOST_CHECK_EQUAL(data_set_by_callback, registered_callback_data);
+
+  data_set_by_callback = initial_data;
+  uint64_t fake_time_set_after_callback_auto_reset = rand();
+  Clock::set_count(fake_time_set_after_callback_auto_reset);
+  uint64_t now_after_callback_auto_reset = Clock::get_count();
+
+  BOOST_CHECK_EQUAL(now_after_callback_auto_reset, fake_time_set_after_callback_auto_reset);
+  BOOST_CHECK_NE(test_callback_counter, fake_time_set_after_callback_auto_reset);
+  BOOST_CHECK_NE(data_set_by_callback, registered_callback_data);
+
+  reset_clock_and_test_data();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
