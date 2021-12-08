26cf9f826664493107db8c86ec27c41df2c6d485
Emmef <git@emmef.org>
2021-12-08 22:50:29 +0100

    Started working on key and value pair reading.

--- include/org-simple/util/Config.h
+++ include/org-simple/util/Config.h
@@ -24,170 +24,118 @@
 #include <cstddef>
 #include <org-simple/util/Characters.h>
 
-namespace org::simple::util {
+namespace org::simple::config {
 
-struct KeyValueCharacters {
-  using Ascii = org::simple::util::Ascii;
+struct Classifier {
+  const org::simple::charClass::Utf8 &classifier =
+      org::simple::charClass::Classifiers::utf8();
 
-  /**
-   * Returns \c true if character \c c can be part of a key name in the widest
-   * sense. By default, all control characters, including the space, are
-   * excluded, as well as the assigment character indicated in \c assignment.
-   * @param c The character to check.
-   * @param assignment The character used for assignment.
-   * @return whether character \c can be used in a key name.
-   */
-  static constexpr bool isKeyCharacter(char c, char assignment) {
-    return !(Ascii::isControlStrict(c) || c == assignment);
+  template <typename codePoint>
+  bool isKeyCharacter(codePoint c, codePoint assignment) const {
+    return c != assignment && classifier.isGraph(c);
   }
-
-  /**
-   * Returns \c true if the character \c c can be part of a key name in the
-   * widest sense. By default, all control characters, including the space, are
-   * excluded, as well as the assigment character '='.
-   * @param c The character to check.
-   * @return whether character \c can be used in a key name.
-   */
-  static constexpr bool isKeyCharacter(char c) {
+  template <typename codePoint> bool isKeyCharacter(codePoint c) const {
     return isKeyCharacter(c, '=');
   }
-
-  /**
-   * Returns \c true when character \c can be part of a value in the widest
-   * sense. By default, this is anything that is a whitespace and not a control
-   * character.
-   * @param c The character to check.
-   * @return whether character \c can be used in a value.
-   */
-  static constexpr bool isValueCharacter(char c) {
-    return Ascii::isWhiteSpace(c) || !Ascii::isControlStrict(c);
+  template <typename codePoint> bool isValueCharacter(codePoint c) const {
+    return (!classifier.isControl(c) || classifier.isBlank(c));
   }
-
-  /**
-   * Returns \c true when character \c can be part of a value in the widest
-   * sense. By default, this is anything that is a whitespace and not a control
-   * character or the character to start a comment provided in \c commentStart.
-   * @param c The character to check.
-   * @param commentStart The character that indicates the start of a comment
-   * @return whether character \c can be used in a value.
-   */
-  static constexpr bool isValueCharacter(char c, char commentStart) {
-    return Ascii::isWhiteSpace(c) || !Ascii::isControlStrict(c);
+  template <typename codePoint>
+  bool isValueCharacter(codePoint c, codePoint commentStart) const {
+    return c != commentStart && isValueCharacter(c);
   }
+};
 
-  class CharacterChecker {
-  public:
-    virtual bool isValid(size_t position, char32_t c) {
-      return !Ascii ::isControl(c);
-    }
-    virtual ~CharacterChecker() = default;
+template <typename codePoint> class KeyOrValueChecker {
+  const Classifier classifier;
 
-    static CharacterChecker *defaultInstance() {
-      static CharacterChecker checker;
-      return &checker;
-    }
-  };
+public:
+  virtual bool validAt(size_t position, codePoint value) const = 0;
 
-  class KeyOrValue {
-  public:
-    virtual CharacterChecker *checker() {
-      return CharacterChecker::defaultInstance();
-    }
-    virtual bool add(size_t index, char c) = 0;
-    virtual const char *get() const = 0;
-    virtual void reset();
-    virtual bool complete() const = 0;
-  };
+  virtual ~KeyOrValueChecker() = default;
 
-  class AbstractKeyOrValue : public KeyOrValue {
-    size_t position_;
-
-  protected:
-    char *name_;
-    size_t length_;
-
-    virtual bool handleState(size_t position, char c) {
-      return checker()->isValid(position, c);
-    }
-
-    AbstractKeyOrValue(char *name, size_t length)
-        : name_(name), length_(length), position_(0) {}
-
-  public:
-    bool add(size_t index, char c) override {
-      if (position_ != index || position_ >= length_) {
-        return false;
+  static const KeyOrValueChecker &defaultInstance() {
+    class Def : public KeyOrValueChecker {
+      bool validAt(size_t, codePoint value) const final {
+        return classifier.template isValueCharacter(value);
       }
-      if (!handleState(position_, c)) {
-        return false;
-      }
-      name_[position_++] = c;
-      name_[position_] = '\0';
-      return true;
-    }
-
-    const char *get() const override { return name_; }
-
-    void reset() override {
-      position_ = 0;
-      name_[0] = '\0';
     };
+    static const Def inst;
+    return inst;
+  }
+};
 
-    bool complete() const override {}
+template <typename codePoint> class AbstractKeyOrValue {
+  size_t pos_ = 0;
+
+protected:
+  virtual bool addAtPosition(size_t position, codePoint c) = 0;
+  [[nodiscard]] virtual const codePoint *getRaw() const = 0;
+  virtual void onReset() {}
+
+public:
+  virtual const KeyOrValueChecker<codePoint> &checker() const {
+    return KeyOrValueChecker<codePoint>::defaultInstance();
   };
 
-  class AbstractUtf8KeyOrValue : public AbstractKeyOrValue {
-    Utf8::Reader reader;
-
-  protected:
-    virtual bool handleState(size_t position, char c) {
-      switch (reader.addGetState(c)) {
-      case Utf8::Reader::State::OK:
-        return checker()->isValid(position, reader.getValueAndReset());
-      case Utf8::Reader::State::READING:
+  bool add(codePoint c) {
+    if (checker().validAt(pos_, c)) {
+      if (addAtPosition(pos_, c)) {
+        ++pos_;
         return true;
-      default:
-        return false;
       }
     }
+    return false;
+  }
 
-    AbstractUtf8KeyOrValue(char *name, size_t length)
-        : AbstractKeyOrValue(name, length) {}
-
-  public:
-    bool complete() const override {
-      return reader.state() == Utf8::Reader::State::OK;
+  [[nodiscard]] const codePoint *get() const {
+    const codePoint *raw = getRaw();
+    if (raw != nullptr && raw[pos_] == '\0') {
+      return raw;
     }
-  };
+    return nullptr;
+  }
 
-  class DefaultKeyOrValue : public AbstractKeyOrValue {
-    CharacterChecker *checker_;
+  void reset() {
+    pos_ = 0;
+    onReset();
+  }
+};
 
-  public:
-    DefaultKeyOrValue(size_t length, CharacterChecker *checker =
-                                         CharacterChecker::defaultInstance())
-        : AbstractKeyOrValue(new char[length + 1], length), checker_() {}
+template <typename codePoint>
+class FixedLengthKeyOrValue : public AbstractKeyOrValue<codePoint> {
+  codePoint * const data_;
+  const size_t length_;
+  const KeyOrValueChecker<codePoint> &checker_;
 
-    CharacterChecker *checker() override { return checker_; }
+protected:
+  bool addAtPosition(size_t position, codePoint c) override {
+    if (length_ <= position) {
+      return false;
+    }
+    data_[position] = c;
+    data_[position + 1] = 0;
+    return true;
+  }
 
-    ~DefaultKeyOrValue() { delete[] this->name_; }
-  };
+  [[nodiscard]] const codePoint *getRaw() const override {
+    return data_;
+  }
 
-  class DefaultUtf8KeyOrValue : public AbstractUtf8KeyOrValue {
-    CharacterChecker *checker_;
+  void onReset() override {}
 
-  public:
-    DefaultUtf8KeyOrValue(
-        size_t length,
-        CharacterChecker *checker = CharacterChecker::defaultInstance())
-        : AbstractUtf8KeyOrValue(new char[length + 1], length), checker_() {}
+public:
+  FixedLengthKeyOrValue(size_t length,
+                        const KeyOrValueChecker<codePoint> &checker)
+      : data_(new codePoint[length]), length_(length), checker_(checker) {}
 
-    CharacterChecker *checker() override { return checker_; }
+  ~FixedLengthKeyOrValue() { delete[] data_; }
 
-    ~DefaultUtf8KeyOrValue() { delete[] this->name_; }
+  const KeyOrValueChecker<codePoint> &checker() const override {
+    return checker_;
   };
 };
 
-} // namespace org::simple::util
+} // namespace org::simple::config
 
 #endif // ORG_SIMPLE_CONFIG_H
