1a5ed58737fab6f1768132c5e02cff8215374410
Emmef <git@emmef.org>
2021-12-08 22:50:12 +0100

    Added character classifications for both ascii and unicode.

--- include/org-simple/util/Characters.h
+++ include/org-simple/util/Characters.h
@@ -22,47 +22,162 @@
  */
 
 #include <algorithm>
+#include <locale>
 #include <org-simple/util/CharEncode.h>
 
-namespace org::simple::util {
+namespace org::simple::charClass {
 
 struct Ascii {
-  static constexpr bool isValid(char c) { return (c & 0x80) == 0; }
-  static constexpr bool isSpace(char c) { return c == ' '; }
-  static constexpr bool isControl(char c) { return c < ' ' || c == 0x7f; }
-  static constexpr bool isControlStrict(char c) {
-    return isSpace(c) || isControl(c);
+  bool isWhiteSpace(char c) const { return std::isspace(c); }
+  bool isLineBreak(char c) const {
+    return (c == '\n' || c == '\r' || c == '\f' || '\v');
   }
-  static constexpr bool isWhiteSpace(char c) { return isSpace(c) || c == '\t'; }
-  static constexpr bool isLineDelimiter(char c) {
-    return c == 'n' || c == '\r';
+  bool isBlank(char c) const { return std::isblank(c); }
+  bool isDigit(char c) const { return std::isdigit(c); }
+  bool isHexDigit(char c) const { return std::isxdigit(c); }
+  bool isLower(char c) const { return std::islower(c); }
+  bool isUpper(char c) const { return std::isupper(c); }
+  bool isAlpha(char c) const { return std::isalpha(c); }
+  bool isAlphaNumeric(char c) const { return std::isalnum(c); }
+  bool isPunctuation(char c) const { return std::ispunct(c); }
+  bool isGraph(char c) const { return std::isgraph(c); }
+  bool isControl(char c) const { return std::iscntrl(c); }
+  /**
+   * If the character represents an opening quote, the corresponding closing
+   * quote is returned, otherwise it returns zero.
+   * @param c The character to test.
+   * @return the corresponding quote or zero if there is none.
+   */
+  char getMatchingQuote(char c) const {
+    if (c == '"' || c == '\'' || c == '`') {
+      return c;
+    }
+    return 0;
   }
-  static constexpr bool isBackspaceOrFeed(char c) {
-    return c == '\b' || c == '\f' || c == '\v';
+  bool isQuote(char c) const { return getMatchingQuote(c) != 0; }
+};
+
+struct Utf8 {
+  const std::locale locale = std::locale("en_US.UTF8");
+  const Ascii ascii;
+  
+  template <typename codePoint> bool isWhiteSpace(codePoint c) const {
+    return std::isspace(c, locale);
   }
-  [[maybe_unused]] static constexpr bool isFormatEffector(char c) {
-    return isWhiteSpace(c) || isLineDelimiter(c) || isBackspaceOrFeed(c);
+  template <typename codePoint> bool isLineBreak(codePoint c) const {
+    return ascii.isLineBreak(c, locale); // we ignore 0x0085, 0x2028, 0x2029
   }
-  static constexpr bool isLowercase(char c) { return c >= 'a' && c <= 'z'; }
-  static constexpr bool isUppercase(char c) { return c >= 'A' && c <= 'Z'; }
-  static constexpr bool isAlpha(char c) {
-    return isLowercase(c) || isUppercase(c);
+  template <typename codePoint> bool isBlank(codePoint c) const {
+    return std::isblank(c, locale);
   }
-  static constexpr bool isDigit(char c) { return c >= '0' && c <= '9'; }
-  static constexpr bool isAlphaNumeric(char c) {
-    return isAlpha(c) || isDigit(c);
+  template <typename codePoint> bool isDigit(codePoint c) const {
+    return std::isdigit(c, locale);
   }
-  static constexpr bool isArithmeticOperator(char c) {
-    return c >= '*' && c <= '-';
+  template <typename codePoint> bool isHexDigit(codePoint c) const {
+    return std::isxdigit(c, locale);
   }
-  static constexpr bool isDash(char c) { return c == '-'; }
-  static constexpr bool isQuote(char c) { return c == '\'' || c == '\"'; }
-  static constexpr bool isQuoteOrTick(char c) { return isQuote(c) || c == '`'; }
-  static constexpr bool isPunctuation(char c) {
-    return c == '!' || c == '.' || c == ',' || c == ':' || c == ';' || c == '?';
+  template <typename codePoint> bool isLower(codePoint c) const {
+    return std::islower(c, locale);
+  }
+  template <typename codePoint> bool isUpper(codePoint c) const {
+    return std::isupper(c, locale);
+  }
+  template <typename codePoint> bool isAlpha(codePoint c) const {
+    return std::isalpha(c, locale);
+  }
+  template <typename codePoint> bool isAlphaNumeric(codePoint c) const {
+    return std::isalnum(c, locale);
+  }
+  template <typename codePoint> bool isPunctuation(codePoint c) const {
+    return std::ispunct(c, locale);
+  }
+  template <typename codePoint> bool isGraph(codePoint c) const {
+    return std::isgraph(c, locale);
+  }
+  template <typename codePoint> bool isControl(codePoint c) const {
+    return std::iscntrl(c, locale);
+  }
+  /**
+   * If the character represents an opening quote, the corresponding closing
+   * quote is returned, otherwise it returns zero.
+   * @param c The code-point to test.
+   * @return the corresponding quote or zero if there is none.
+   */
+  template <typename codePoint> char getMatchingQuote(codePoint c) const {
+    codePoint r = ascii.getMatchingQuote(c);
+    if (r != 0) {
+      return r;
+    }
+    if (r < 127) {
+      return 0;
+    }
+    if (c == 0x002019 || c == 0x00201D || c == 0x0000BB) {
+      return c;
+    }
+    switch (c) {
+    case 0x00201A:
+      return 0x002018;
+    case 0x00201E:
+      return 0x00201C;
+    case 0x0000AB:
+      return 0x0000BB;
+    case 0x002039:
+      return 0x00203A;
+    case 0x00201E:
+      return 0x00201D;
+    case 0x0000BB:
+      return 0x0000AB;
+    case 0x00300C:
+      return 0x00300D;
+    case 0x00FE41:
+      return 0x00FE42;
+    case 0x00300E:
+      return 0x00300F;
+    case 0x00FE43:
+      return 0x00FE44;
+    case 0x00201C:
+      return 0x00201D;
+    case 0x002018:
+      return 0x002019;
+    case 0x00300A:
+      return 0x00300B;
+    case 0x003008:
+      return 0x003009;
+    }
+    // There is ambiguity, so the single polish quote, U+201A does not return
+    // 0x2019 but 0x2018.
+    return 0;
+  }
+  template <typename codePoint> bool isQuote(codePoint c) const {
+    return getMatchingQuote(c) != 0;
+  }
+
+  bool isWhiteSpace(char c) const { return ascii.isWhiteSpace(c); }
+  bool isLineBreak(char c) const { return ascii.isLineBreak(c); }
+  bool isBlank(char c) const { return ascii.isBlank(c); }
+  bool isDigit(char c) const { return ascii.isDigit(c); }
+  bool isHexDigit(char c) const { return ascii.isHexDigit(c); }
+  bool isLower(char c) const { return ascii.isLower(c); }
+  bool isUpper(char c) const { return ascii.isUpper(c); }
+  bool isAlpha(char c) const { return ascii.isAlpha(c); }
+  bool isAlphaNumeric(char c) const { return ascii.isAlphaNumeric(c); }
+  bool isPunctuation(char c) const { return ascii.isPunctuation(c); }
+  bool isGraph(char c) const { return ascii.isGraph(c); }
+  bool isControl(char c) const { return ascii.isControl(c); }
+  char getMatchingQuote(char c) const;
+  bool isQuote(char c) const { return ascii.isQuote(c); }
+};
+struct Classifiers {
+  static const Ascii &ascii() {
+    static const Ascii ascii;
+    return ascii;
+  }
+  static const Utf8 &utf8() {
+    static const Utf8 utf8{};
+    return utf8;
   }
 };
 
-} // namespace org::simple::util
+} // namespace org::simple::charClass
 
 #endif // ORG_SIMPLE_CHARACTERS_H
