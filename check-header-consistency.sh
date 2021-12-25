3672e52c1a16df8861e08c8a3fff9d9bd3785e94
Emmef <git@emmef.org>
2021-12-26 00:16:32 +0100

    check-header-consistency.sh: small improvements, but lowered priority to improve further.

--- check-header-consistency.sh
+++ check-header-consistency.sh
@@ -1,41 +1,70 @@
 #!/bin/bash
 
+pushd `dirname "$0"`
+
+error_exit() {
+  popd
+  exit $1
+}
+
 checkSanity() {
   local fullPath
   local pathName
   local nameSpace
   local fileName
   local tempDirectory
-  local fileNameInheader
-  
+  local correctNameInHeader
+  local fileLineInHeader
+
   fullPath="$1"
   pathName=$(dirname "$fullPath")
   fileName=$(basename "$fullPath")
-  
+
 # Strip include from filename
   pathName=${pathName/.\/include\//}
   nameSpace=${pathName//\//::}
   nameSpace=${nameSpace//-/::}
-  
+
   tempDirectory="/tmp/replace/include/${pathName}"
-  
+
 #  echo "File $fileName Path $pathName NameSpace $nameSpace"
-  fileNameInheader="${pathName}/${fileName}"
-  if ! grep -E " * $fileNameInheader" "$fullPath" >/dev/null
+  correctNameInHeader="${pathName}/${fileName}"
+  fileLineInHeader=$(grep -E -A1 "^/\\*s*\$" "$fullPath" | grep -Ei "^ \* [-_A-Z0-9/]+\.h.*" "$fullPath")
+
+#  echo "File line: '$fileLineInHeader'"
+
+  if ! echo "$fileLineInHeader" | grep -E "^ \\* $correctNameInHeader\$" >/dev/null
   then
-    echo -e "File ${fullPath} should use following file-name:\n\t${fileNameInheader}" >&2
+    echo -e "File ${fullPath} should use following file-name:\n\t${correctNameInHeader}" >&2
   fi
-  
+
   if grep -E "^namespace\s+[a-z0-9A-Z:]+\s+{" "${fullPath}" >/dev/null
-  then 
+  then
     if ! grep -E "^namespace\s+${nameSpace}\s+\{" "${fullPath}" >/dev/null
-    then 
+    then
       echo -e "File ${fullPath} should use following namespace:\nnamespace ${nameSpace} {\n} // namespace ${nameSpace}" >&2
     fi
   fi
-  
+#
+#  if [ ! -d "${tempDirectory}" ]
+#  then
+#    if ! mkdir -p "${tempDirectory}"
+#    then
+#      echo "Cannot create temporary directory: ${tempDirectory}" >&2
+#      exit 1
+#    fi
+#  fi
+#  if ! cp "$fullPath" "${tempDirectory}/$fileName"
+#  then
+#    echo "Cannot make copy ${tempDirectory}/${fileName}" >&2
+#    exit 1
+#  fi
+
+
+
 }
 
 
- for name in `find .| grep -Ei './include/.*\.h' | sort` ; do  checkSanity "$name" ; done
- 
+for name in `find .| grep -Ei './include/.*\.h' | sort` ; do  checkSanity "$name" ; done
+
+popd
